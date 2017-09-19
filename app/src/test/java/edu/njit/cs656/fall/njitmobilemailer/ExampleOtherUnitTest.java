056ae2507eddc24528cac9e8ec8842fc6e2f3cad
Eugene Turgil <eugeneturgil@gmail.com>
2017-09-18 21:38:27 -0400

    seeing out other files work with this strategy

---
+++ app/src/test/java/edu/njit/cs656/fall/njitmobilemailer/ExampleOtherUnitTest.java
@@ -0,0 +1,27 @@
+package edu.njit.cs656.fall.njitmobilemailer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleOtherUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 4);
+    }
+
+    @Test
+    public void addition_isIncorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void addition_anotherTest() throws Exception {
+        assertEquals(5, 2 + 2);
+    }
+}
