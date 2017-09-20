aa4a5f6deb585b071280f7b6f870a38f24e9d551
turgile <et24@njit.edu>
2017-09-19 23:40:17 -0400

    added some more content to the file, hopefully will be done with it tomorrow.

--- app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/email/Smtp.java
+++ app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/email/Smtp.java
@@ -4,14 +4,19 @@ package edu.njit.cs656.fall.njitmobilemailer.email;
  * Created by Eugene on 9/19/2017.
  */
 
+import android.util.Base64;
 import android.util.Log;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.nio.channels.Pipe;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -43,8 +48,54 @@ public class Smtp {
         // then data
     }
 
-    public void sendMessage() {
+    private boolean authenticateUser(String username, String password, BufferedWriter writer, BufferedReader reader) {
+        StringBuilder authenticationString = new StringBuilder();
+        authenticationString.append("\\000");
+        authenticationString.append(username.substring(0, username.indexOf('@') -1));
+        authenticationString.append(username.substring( username.indexOf('@'), username.length()));
+        authenticationString.append("\\000");
+        authenticationString.append(password);
 
+        byte[] stringArray = new byte[authenticationString.length()];
+        String base64EncodedAuthenticationString = Base64.encodeToString(authenticationString.toString().getBytes(), 0);
+
+        try {
+            writer.write(String.format("auth plain %s", base64EncodedAuthenticationString));
+            String response = reader.readLine();
+
+            if(response.indexOf("Accepted") < 0 ) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+    }
+    public String sendMail(String to, String from, String data) {
+        try {
+            InputStream readStream = mailServerSocket.getInputStream();
+            InputStreamReader readerStream = new InputStreamReader(readStream);
+            BufferedReader reader = new BufferedReader(readerStream);
+
+            OutputStream writeStream = mailServerSocket.getOutputStream();
+            OutputStreamWriter writerStream = new OutputStreamWriter(writeStream);
+            BufferedWriter writer = new BufferedWriter(writerStream);
+
+            String message = reader.readLine();
+            writer.write("EHLO njit.edu");
+
+            reader.close();
+            readerStream.close();
+            readStream.close();
+
+            writer.close();
+            writerStream.close();
+            writeStream.close();
+        } catch ( IOException e ) {
+            return null;
+        }
+        return null;
     }
 
     public boolean DestroyServer() {
