8ce5b772e0b165d57abf6c1c043c78ddba99ad03
Eugene Turgil <eugeneturgil@gmail.com>
2017-10-12 20:47:23 -0400

    added unit test

---
+++ app/src/test/java/edu/njit/cs656/fall/njitmobilemailer/MailTest.java
@@ -0,0 +1,36 @@
+package edu.njit.cs656.fall.njitmobilemailer;
+
+/**
+ * Created by Eugen on 10/12/2017.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.njit.cs656.fall.njitmobilemailer.email.Mail;
+
+import static junit.framework.Assert.assertEquals;
+
+public class MailTest {
+
+    private Mail email;
+
+    @Before
+    public void initialize() {
+        email = new Mail();
+    }
+
+    @Test
+    public void testToClient() {
+        final String testText = "test@test.com";
+        email.setToClient(testText);
+        assertEquals(testText, email.getToClient());
+    }
+
+    @Test
+    public void testSubject() {
+        final String testText = "TEST";
+        email.setSubject(testText);
+        assertEquals(testText, email.getSubject());
+    }
+}
