c5249a39d11f4c126876768127c97bace210e25d
Eugene Turgil <eugeneturgil@gmail.com>
2017-10-12 21:35:50 -0400

    testing sennd email using javax mail code

--- app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/email/Send.java
+++ app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/email/Send.java
@@ -12,4 +12,34 @@ import javax.activation.*;
 
 public class Send {
 
+    private static final String mx = "smtp.gmail.com";
+    private static final String host = "njit.edu";
+
+    public static void Email(Mail letter) {
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.user", "");
+        properties.setProperty("mail.password", "");
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress(letter.getFromClient()));
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(letter.getToClient()));
+
+            message.setSubject(letter.getSubject());
+
+            BodyPart messageBody = new MimeBodyPart();
+
+            messageBody.setText(letter.getMessage());
+
+            Transport.send(message);
+        } catch(MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
 }
