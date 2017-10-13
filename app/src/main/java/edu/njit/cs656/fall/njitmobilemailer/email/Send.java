b96693f2de61940cccb9f4c0ebc902cb5fd59ccc
Eugene Turgil <eugeneturgil@gmail.com>
2017-10-13 18:13:05 -0400

    secure smtp, however still need to implement secure authentication.

--- app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/email/Send.java
+++ app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/email/Send.java
@@ -13,14 +13,23 @@ import javax.activation.*;
 public class Send {
 
     private static final String host = "smtp.gmail.com";
+    private static final String username = "et24@njit.edu";
+    private static final String password = "test"; // TODO: cannot store password in plain text
 
     public static void Email(Mail letter) {
-        Properties properties = System.getProperties();
+        Properties properties = new Properties();
         properties.setProperty("mail.smtp.host", host); // TODO: 10/12/2017 Add SSL/TLS port
-        properties.setProperty("mail.user", "");
-        properties.setProperty("mail.password", "");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.port", "587");
 
-        Session session = Session.getDefaultInstance(properties);
+        Session session = Session.getDefaultInstance(properties,
+                new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
 
         try {
             MimeMessage message = new MimeMessage(session);
