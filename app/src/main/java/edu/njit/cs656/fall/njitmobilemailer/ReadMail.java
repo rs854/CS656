f4c1d63fd122a41ba00bf531fd2e431b59be7696
Eugene <eugeneturgil@gmail.com>
2017-10-17 21:30:17 -0400

    added more basic activities, still need to be implemented

---
+++ app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/ReadMail.java
@@ -0,0 +1,13 @@
+package edu.njit.cs656.fall.njitmobilemailer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadMail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_mail);
+    }
+}
