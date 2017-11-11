12be47fa1d86bf042c735e70a3d74bbda9cf5ede
Eugene Turgil <eugeneturgil@gmail.com>
2017-11-11 11:05:30 -0500

    I have send email very loosely working. The UI definitely needs an overall, and we probably need some error handling as well.

--- app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/SendMail.java
+++ app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/SendMail.java
@@ -1,13 +1,53 @@
 package edu.njit.cs656.fall.njitmobilemailer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import edu.njit.cs656.fall.njitmobilemailer.email.Mail;
+import edu.njit.cs656.fall.njitmobilemailer.email.Send;
 
 public class SendMail extends AppCompatActivity {
+
+
+    private String getTo() {
+        EditText item = (EditText) findViewById(R.id.editText2);
+        return item.getText().toString();
+    }
+
+    private String getBody() {
+        EditText item = (EditText) findViewById(R.id.editText3);
+        return item.getText().toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_mail);
+
+        View sendButton = findViewById(R.id.button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Mail letter = new Mail();
+                        letter.setSubject("TEST SUBJECT");
+                        letter.setMessage(((EditText) findViewById(R.id.editText3)).getText().toString());
+                        try {
+                            letter.setFromClient("et24@njit.edu");
+                            letter.setToClient(((EditText) findViewById(R.id.editText2)).getText().toString());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        Send.Email(letter);
+                    }
+                }).start();
+
+            }
+        });
     }
 }
