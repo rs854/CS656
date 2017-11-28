571efc8c4322d0026d819a89e03398e1783656ae
Robert Smith <robsmith@ofsoptics.com>
2017-11-28 15:01:00 -0500

    Toolbar menus

--- app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/SendMail.java
+++ app/src/main/java/edu/njit/cs656/fall/njitmobilemailer/SendMail.java
@@ -2,6 +2,9 @@ package edu.njit.cs656.fall.njitmobilemailer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -10,6 +13,7 @@ import edu.njit.cs656.fall.njitmobilemailer.email.Send;
 
 public class SendMail extends AppCompatActivity {
 
+    private Toolbar toolbar;
 
     private String getTo() {
         EditText item = (EditText) findViewById(R.id.to_editText);
@@ -22,20 +26,43 @@ public class SendMail extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.send_mail_menu, menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_mail);
 
-        View sendButton = findViewById(R.id.button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+//        View sendButton = findViewById(R.id.action_send);
+//        sendButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_send:
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
 
                         Mail letter = new Mail();
-                        letter.setSubject("TEST SUBJECT");
+                        letter.setSubject(((EditText) findViewById(R.id.subject_editText)).getText().toString());
                         letter.setMessage(((EditText) findViewById(R.id.body_editText)).getText().toString());
                         try {
                             letter.setFromClient("et24@njit.edu");
@@ -46,8 +73,11 @@ public class SendMail extends AppCompatActivity {
                         Send.Email(letter);
                     }
                 }).start();
+                break;
+            default:
+                break;
+        }
 
-            }
-        });
+        return true;
     }
 }
